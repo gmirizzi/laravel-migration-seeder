6475ecc16bcfeec25a1dda323c9d4225c8bba879
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-05-06 15:40:26 +0200

    implementato il faker

--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Train;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -9,8 +11,21 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 100; $i++) { 
+            $newTrain= new Train();
+            $newTrain->azienda=$faker->company();
+            $newTrain->stazione_di_partenza;
+            $newTrain->stazione_di_arrivo;
+            $newTrain->date;
+            $newTrain->orario_di_partenza;
+            $newTrain->orario_di_arrivo;
+            $newTrain->code;
+            $newTrain->nr_carrozze;
+            $newTrain->on_time;
+            $newTrain->canceled;
+            $newTrain->save();
+        }
     }
 }
